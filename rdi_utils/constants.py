e065cc50d36d45e94046443ccf954584a07d17c5
pingle14 <prachi.ingle@utexas.edu>
2023-02-01 20:53:58 -0600

    updated gdi to rdi

--- rdi_utils/constants.py
+++ rdi_utils/constants.py
@@ -1,4 +1,4 @@
-page_title = "US Group Disparity Index Dashboard"
+page_title = "US Racial Disparity Index Dashboard"
 states = {
     # "All": "All",
     "AK": "Alaska",
@@ -54,8 +54,8 @@ states = {
     "WY": "Wyoming",
 }
 descriptions = {
-    "header": "This site seeks to explore the relationship between individuals' socioeconomic characteristics and their likelihood of homeownership. Specifically we define a **Group Disparity Index (RDI)** that quantifies group inequality in an outcome gap in the homeownership problem space.",
+    "header": "This site seeks to explore the relationship between individuals' socioeconomic characteristics and their likelihood of homeownership. Specifically we define a **Racial Disparity Index (RDI)** that quantifies racial inequality in an outcome gap in the homeownership problem space.",
     "map": "In this page, users can visualize how specific socioeconomic characteristics change for PUMA Areas across the US for a specific year. According to the [US Census](https://www.census.gov/programs-surveys/geography/guidance/geo-areas/pumas.html), Public Use Microdata Areas (PUMAs) are statistical regions that partition each state into areas with at least 100,000 people.",
-    "timeline": "In this page, users can visualize how the Group Disparity Index (RDI) of a particular state changes over time.",
+    "timeline": "In this page, users can visualize how the Racial Disparity Index (RDI) of a particular state changes over time.",
     "model": "In this page, users can run our models with their own sample data. Please refer to the sample input file to see how to format the uploaded CSV that we will plug into our models.",
 }
